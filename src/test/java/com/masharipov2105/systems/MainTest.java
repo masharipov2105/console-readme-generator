2a4394a851f462b6580e5c78439f45ed184d5daa
masharipov2105 <masharipov2105@gmail.com>
2026-09-04 11:38:32 -0400

    first commit

---
+++ src/test/java/com/masharipov2105/systems/MainTest.java
@@ -0,0 +1,14 @@
+package com.masharipov2105.systems;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void testExample(){
+
+        assertEquals(10, 5 + 5);
+        System.out.println("Test success run");
+    }
+}
