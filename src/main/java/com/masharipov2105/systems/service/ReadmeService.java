fe0859e8d79b61d918c8438c44273cdab18250a1
masharipov2105 <masharipov2105@gmail.com>
2026-09-21 18:09:30 +0500

    created ReadmeService interface

---
+++ src/main/java/com/masharipov2105/systems/service/ReadmeService.java
@@ -0,0 +1,9 @@
+package com.masharipov2105.systems.service;
+
+import com.masharipov2105.systems.model.RequestModel;
+
+
+public interface ReadmeService{
+
+	boolean uploadProjectData(RequestModel model);
+}
