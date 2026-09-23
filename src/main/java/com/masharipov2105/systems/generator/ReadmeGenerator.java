da76cb3d099d5bb0f81d2fa132155e22747b5de1
masharipov2105 <masharipov2105@gmail.com>
2026-09-23 09:03:10 +0500

    Create ReadmeGenerator interface

---
+++ src/main/java/com/masharipov2105/systems/generator/ReadmeGenerator.java
@@ -0,0 +1,9 @@
+package com.masharipov2105.systems.generator;
+
+import com.masharipov2105.systems.model.ReadmeModel;
+
+public interface ReadmeGenerator{
+
+
+	boolean generate(ReadmeModel finalModel);
+}
